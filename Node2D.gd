995bdcf4953b2b5f4347f9d75ce74d0b979f5da1
Petia Davidova <petia.davidova@student.manchester.ac.uk>
2017-08-05 22:31:25 +0300

    timer added

--- Node2D.gd
+++ Node2D.gd
@@ -8,7 +8,9 @@ var count = 0
 
 func _ready():
 	get_node("number").set_text(String(0))
-	while count < 10000:
+	while count <= 10000:
+		get_node("Timer").set_wait_time(5)
+		get_node("Timer").start()
+		#if get_node("Timer").set_wait_time(5.0):
+		get_node("number").set_text(String(count))
 		count+=1
-		if count % 10 == 0:
-			get_node("number").set_text(String(count))
