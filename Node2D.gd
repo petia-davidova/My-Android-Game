13681eb7c8bb149ceb5dfa23bb84bd6b874474b4
Petia Davidova <petia.davidova@student.manchester.ac.uk>
2017-08-05 22:40:16 +0300

    script on the wrong node

--- Node2D.gd
+++ Node2D.gd
@@ -4,13 +4,6 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
-var count = 0
 
 func _ready():
-	get_node("number").set_text(String(0))
-	while count <= 10000:
-		get_node("Timer").set_wait_time(5)
-		get_node("Timer").start()
-		#if get_node("Timer").set_wait_time(5.0):
-		get_node("number").set_text(String(count))
-		count+=1
+	pass
