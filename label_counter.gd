4eb7b66061026b18ea4fd8a1f06bfeaffd541f33
Petia Davidova <petia.davidova@student.manchester.ac.uk>
2017-08-05 23:02:01 +0300

    countdown works, label changes properly, label not centered

---
+++ label_counter.gd
@@ -0,0 +1,13 @@
+extends Label
+
+var countdown_time = 10000
+var rounded_time = 0
+
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	countdown_time -= delta
+	rounded_time = round(countdown_time)
+	set_text(str(rounded_time))
