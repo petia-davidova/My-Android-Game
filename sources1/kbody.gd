6e08abc844e59ee735799ff17ef0200fd758542c
Petia Davidova <petia.davidova@student.manchester.ac.uk>
2017-08-19 22:38:14 +0300

    finally controlling the ball with tilting YAY

--- sources1/kbody.gd
+++ sources1/kbody.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 # variable for gravity - constant
-const GRAVITY = Vector2(0, 10)
-
+const GRAVITY = 10.0
+const WALK_SPEED = 400
+var gyro = Input.get_gyroscope()
 # variable for ball's velocity
 var velocity = Vector2()
 
@@ -10,5 +11,13 @@ func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	velocity += GRAVITY * delta
-	move(velocity)
+	velocity.y += GRAVITY * delta
+	if (Input.get_accelerometer().x > 0):
+        velocity.x = -WALK_SPEED
+	elif (Input.get_accelerometer().x < 0):
+        velocity.x =  WALK_SPEED
+	else:
+        velocity.x = 0
+
+	var motion = velocity * delta
+	move(motion)
