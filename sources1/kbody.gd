36a464f194f972672589b8f7c51a3366e7a31118
Petia Davidova <petia.davidova@student.manchester.ac.uk>
2017-08-20 21:39:42 +0300

    ball angle of moion calculated with trigonometry

--- sources1/kbody.gd
+++ sources1/kbody.gd
@@ -1,11 +1,11 @@
-extends KinematicBody2D
+ extends KinematicBody2D
 
 # variable for gravity - constant
 const GRAVITY = 9.8
 const WALK_SPEED = 400
 var accel = Input.get_accelerometer()
 var initial_motion = Vector2(18, -10)
-var motion = Vector2(18, -10)
+var motion = Vector2(10, -10)
 var move_x = motion.x
 var move_y = motion.y
 # variable for ball's velocity
@@ -25,18 +25,21 @@ func _fixed_process(delta):
 #
 #	var motion = velocity * delta
 #	motion = Vector2(8, 8)
-#	motion = initial_motion
+
+	move(motion)
 
 	var angle_rad = asin(Input.get_accelerometer().x/GRAVITY)
 	var angle_deg = angle_rad * (180/PI)
-	get_parent().get_node("Label").set_text(str(round(angle_deg)))
+	get_parent().get_node("accel-label").set_text(str(round(angle_deg)))
 #	print ("before: " + str(motion.x))
-#	move(motion)
-#	
-#	if is_colliding():
+
+	var ball_deg = atan(motion.x/motion.y) * (180/PI)
+	get_parent().get_node("ball-angle-label").set_text(str((ball_deg)))
+	
+	if is_colliding():
 #		var current_x = accel.x
 #		motion.x = current_x
 #		print ("after: " + str(motion.x))
-#		motion.x = -motion.x
-#		move(motion)
+		motion.x = -motion.x
+		move(motion)
 
